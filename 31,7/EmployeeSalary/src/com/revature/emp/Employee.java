53bf9bcfcd99bf90f2253849d23a91f248fdcd35
jayesh <jayesh@gmail.com>
2024-08-03 22:24:05 +0530

    second

---
+++ 31,7/EmployeeSalary/src/com/revature/emp/Employee.java
@@ -0,0 +1,24 @@
+package com.revature.emp;
+
+public class Employee {
+	
+	private int id;
+	private String name;
+	private int salary;
+	public Employee(int id, String name, int salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	public Employee() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+	
+	
+	
+}
---
+++ 31,7/EmployeeSalary/src/com/revature/emp/EmployeeSalary.java
@@ -0,0 +1,9 @@
+package com.revature.emp;
+
+public class EmployeeSalary {
+	public static void main(String[] args)
+	{
+		Employee e=new Employee(101,"jayesh",21000);
+		System.out.println(e.toString());
+	}
+}
